d3c13eb939c7ba7851dc2fb6dabfb7515c926291
Tony Luisi <t.luisi@gmail.com>
2017-07-21 23:58:00 +1200

    first csharp attempt

---
+++ 1-simple-array-sum/CSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("../input/1.txt");
+            string[] stringArray = lines[1].Split(' ');
+            int[] intArray = new int[stringArray.Length];
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                intArray[i] = Int32.Parse(stringArray[i]);
+            }
+            Console.WriteLine(SumArray(intArray));
+        }
+
+        static int SumArray(int[] intArray)
+        {
+            int sum = 0;
+            foreach (int number in intArray)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+}
