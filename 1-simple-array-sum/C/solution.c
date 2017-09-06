dccd8ccea72b8bcb92834a6352c880105bcf3c85
Tony Luisi <t.luisi@gmail.com>
2017-09-07 08:30:15 +1200

    refactored c solution

--- 1-simple-array-sum/C/solution.c
+++ 1-simple-array-sum/C/solution.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int input[] = {1, 2, 3, 4, 10, 11};
+
+int sum_array(int *number_array, int array_size) 
+{
+  int sum = 0;
+  for (int i = 0; i < array_size; i++)
+  {
+    sum += number_array[i];
+  }
+  return sum;
+
+}
+
 int main() {
   // Read File In
   static const char filename[] = "../input/1.txt";
   FILE *file;
   file = fopen (filename, "r");
+  int my_sum = sum_array(input, 6);
+  printf("MY SUM: %i", my_sum);
 
   if ( file != NULL )
   {
@@ -21,11 +36,7 @@ int main() {
         fscanf(file, "%s", number);
         number_array[array_position++] = atoi(number);
       }
-      int sum = 0;
-      for (int i = 0; i < array_size; i++)
-      {
-        sum = sum +  number_array[i];
-      }
+      int sum = sum_array(number_array, array_size);
       printf("SUM: %i", sum);
 
     }
@@ -35,6 +46,8 @@ int main() {
   }
   return 0;
 }
+
+
 
 // LEARNINGS
 // Reading a file line by line is difficult
@@ -46,3 +59,4 @@ int main() {
 // functions can't return arrays
 // arrays aren't first class in c
 // array size needs to be saved to local variable
+// arrays need to be passed into functions as a pointer
